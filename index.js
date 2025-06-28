b9b0f35272e6797a6ec89850a4b4d2e6f56d5c04
Sandeep <sandeepkamma100@gmail.com>
2025-06-28 21:21:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function rollDice(){
     for(let i=0;i<numOfDice;i++){
         const value = Math.floor(Math.random()*6)+1;
         values.push(value);
-        images.push(`<img src="dice_images/${value}.png" alt="dice${value}" width = "150" height = "150">`);
+        images.push(`<img src="./${value}.png" alt="dice${value}" width = "150" height = "150">`);
     }
     diceResult.textContent=`dice:${values.join(`,`)}`;
     diceImages.innerHTML = images.join(` `);
