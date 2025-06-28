cbc952225aa08eb468560ff489ef66862d2032c9
Sandeep <sandeepkamma100@gmail.com>
2025-06-28 20:52:49 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function rollDice(){
+    const numOfDice = document.getElementById("ip").value;
+    const diceResult = document.getElementById("diceValues");
+    const diceImages = document.getElementById("diceImages");
+
+    const values=[];
+    const images = [];
+
+    for(let i=0;i<numOfDice;i++){
+        const value = Math.floor(Math.random()*6)+1;
+        values.push(value);
+        images.push(`<img src="dice_images/${value}.png" alt="dice${value}" width = "150" height = "150">`);
+
+
+    }
+
+    diceResult.textContent=`dice:${values.join(`,`)}`;
+    diceImages.innerHTML = images.join(` `);
+
+
+
+
+}
